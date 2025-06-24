67b5bf32d61f6054f31f3f9d13367c767ef5e93e
LirBast <liroybast@gmail.com>
2025-06-23 22:50:12 -0400

    Resuelvo conflictos

---
+++ modulo 3/ejercicioclaseslunes.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# serie=pd.Series([10,20,30], index=["b0","b1","c"])
+# print(serie["b0"])
+
+# print(serie)
+
+
+materias=pd.Series([50,40,60], index=["matematicas", "ciencias", "lenguas"])
+print(materias)
