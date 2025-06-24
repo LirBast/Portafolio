4381acc46288fcfe234cdbe61d667c98bc17f8f4
LirBast <liroybast@gmail.com>
2025-06-23 23:01:12 -0400

    Resuelvo conflictos

--- modulo 3/ejercicioclaseslunes.py
+++ modulo 3/ejercicioclaseslunes.py
@@ -1,10 +1,15 @@
 import pandas as pd
 
-# serie=pd.Series([10,20,30], index=["b0","b1","c"])
-# print(serie["b0"])
+serie=pd.Series([10,20,30], index=["b0","b1","c"])
+print(serie["b0"])
 
-# print(serie)
+print(serie)
 
+print(serie.mean)
 
 materias=pd.Series([50,40,60], index=["matematicas", "ciencias", "lenguas"])
 print(materias)
+
+datos2={"matematicas":50, "ciencias":40, "lenguas":60}       ###Diccionario las claves son las materias y las notas son los valores
+materias2=pd.Series(datos2)
+print(materias2)
